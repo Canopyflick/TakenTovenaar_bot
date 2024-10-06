ac780c6383e7eda08729f71e12153481e435ed93
Ben ten Berge <tenbergeben@gmail.com>
2024-10-06 21:23:47 +0200

    3.5

--- TelegramBot_Takentovenaar.py
+++ TelegramBot_Takentovenaar.py
@@ -605,7 +605,7 @@ async def handle_regular_message(update, context):
     
         # Check the outcome of the dice roll
         rolled_value = dice_message.dice.value
-        await asyncio.sleep(4)
+        await asyncio.sleep(3.5)
     
         # Give a reply based on the rolled value
         if rolled_value == user_guess:
